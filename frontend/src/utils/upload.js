181bad5edf8e02451527a46a2a051e9df13029d5
lulin <2024080904024@std.uestc.edu.cn>
2026-07-15 03:25:11 +0800

    资源文件（图片、视频、音频）设计完成

--- frontend/src/utils/upload.js
+++
@@ -1,9 +0,0 @@
-export const buildFormData = (data) => {
-    const fd = new FormData()
-    Object.keys(data).forEach(key => {
-        if (data[key] !== undefined && data[key] !== null) {
-            fd.append(key, data[key])
-        }
-    })
-    return fd
-}
