2434c87d43bc5cd389d7a4c08e6916b59083a4e8
zhou <cav_prim@outlook.com>
2026-07-15 09:28:54 +0800

    测试tmp

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,11 +6,11 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-// 开发环境下引入模拟数据用于测试
-// 开发环境下进行前后端连通测试时注释掉这部分
-if (import.meta.env.DEV) {
-  import('./mock')
-}
+// // 开发环境下引入模拟数据用于测试
+// // 开发环境下进行前后端连通测试时注释掉这部分
+// if (import.meta.env.DEV) {
+//   import('./mock')
+// }
 
 const app = createApp(App)
 
